b672933abb99e2c5ae9b52e4bee3d11a34f6bc08
Kevin O. Cruz <kocruz.24@gmail.com>
2022-09-01 12:10:55 -0400

    [61] Remaining Components

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,22 +1,16 @@
-import { Component } from 'react';
 import Card from '../card/card.component';
 import './card-list.styles.css';
 
-class CardList extends Component {
-	render() {
-		console.log(this.props);
-		console.log('render from CardList');
+const CardList = (props) => {
+	const { monsters } = props;
 
-		const { monsters } = this.props;
-
-		return (
-			<div className="card-list" key={monsters.id}>
-				{monsters.map((monster) => {
-					return <Card key={monster.id} monster={monster} />;
-				})}
-			</div>
-		);
-	}
-}
+	return (
+		<div className="card-list" key={monsters.id}>
+			{monsters.map((monster) => {
+				return <Card key={monster.id} monster={monster} />;
+			})}
+		</div>
+	);
+};
 
 export default CardList;
--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,19 +1,16 @@
-import { Component } from 'react';
 import './search-box.styles.css';
 
-class SearchBox extends Component {
-	render() {
-		const { onChangeHandler, className, placeholder } = this.props;
+const SearchBox = (props) => {
+	const { onChangeHandler, className, placeholder } = props;
 
-		return (
-			<input
-				className={`search-box ${className}`}
-				type="search"
-				placeholder={placeholder}
-				onChange={onChangeHandler}
-			></input>
-		);
-	}
-}
+	return (
+		<input
+			className={`search-box ${className}`}
+			type="search"
+			placeholder={placeholder}
+			onChange={onChangeHandler}
+		></input>
+	);
+};
 
 export default SearchBox;
